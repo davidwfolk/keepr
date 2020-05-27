6028b2cd20a4503666cb9523a9017f413258c473
David Folk <davidwfolk@gmail.com>
2020-05-27 14:16:48 -0600

    updated styling

--- wwwsrc/src/components/navbar.vue
+++ wwwsrc/src/components/navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <router-link class="navbar-brand" :to="{ name: 'home' }">Keepr</router-link>
+    <router-link class="navbar-brand" :to="{ name: 'home' }">Par-adise</router-link>
     <button
       class="navbar-toggler"
       type="button"
